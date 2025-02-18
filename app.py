99f2049dfe837bc872059e93ba880c25daa5bb4d
Fouad Gantri <fouad@gantri.dev>
2025-02-18 14:58:27 +0100

    add basic ai system instructions

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
 from dotenv import load_dotenv
-from utility import send_message
 from flask import Flask, request
 import os
 from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
+import google.generativeai as genai
 
 load_dotenv(".env")
 
 account_sid = os.getenv("ACCOUNT_SID")
 auth_token = os.getenv("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
+genai.configure(api_key=os.getenv("AI_API_KEY"))
 
-def main():
-    send_message(client=client, message="Initial Message")
+# Create the model
+generation_config = genai.GenerationConfig(
+    temperature=1,
+    top_p=0.95,
+    top_k=40,
+    max_output_tokens=8192,
+    response_mime_type="text/plain",
+)
+
+with open("system_instruction.txt", "r") as f:
+    system_instruction = f.read()
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash",
+  generation_config=generation_config,
+  system_instruction=system_instruction
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
 
 
 app = Flask(__name__)
@@ -20,16 +40,27 @@ app = Flask(__name__)
 
 @app.route("/hook/incoming_message", methods=["POST"])
 def on_incoming_message():
+    phone_number = request.form['From']
     incoming_message = request.form['Body']
 
-    # TODO process message
+    print(f"Received message: {incoming_message} from e{phone_number}e")  # Add logging
+    # process message
+    ai_response = chat_session.send_message(incoming_message)
 
-    # generate response
-    response = MessagingResponse()
-    response.message(f"You told me: \"{incoming_message}\". I'll reach out to you soon.")
-    return str(response)
+    print(f"AI Response: {ai_response.text}")  # Log AI the response
+
+    try:
+        message = client.messages.create(
+            body=ai_response.text,
+            from_=f'whatsapp:{os.getenv("TWILIO_PHONE")}',
+            to=phone_number
+        )
+        print(f"Message sent to {phone_number}. SID: {message.sid}")  # Log message SID for tracking
+    except Exception as e:
+        print(f"Error sending message via Twilio: {e}")
+
+    return "" # endpoint has to return something valid other than None
 
 
 if __name__ == "__main__":
-    main()
     app.run(port=5000)
--- utility.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def send_message(client, message):
-    message = client.messages.create(
-        from_=f"whatsapp:{os.getenv("TWILIO_PHONE")}",
-        body=message,
-        to=f"whatsapp:{os.getenv("MY_PHONE")}"
-    )
-    return message.sid
